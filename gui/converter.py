73626d172307cb700aab8d4c66920566d2a6edfc
email <tlsqja1120@gmail.com>
2021-10-08 13:20:03 +0900

    gui 추가파일

--- gui/converter.py
+++ gui/converter.py
@@ -6,11 +6,14 @@ class App:
         frame.pack()
 
         Label(frame, text="deg C").grid(row=0, column=0)
-        Button(frame, text="변환").grid(row=0, column=0)
+        Button(frame, text="변환", command=self.convert).grid(row=0, column=0)
 
+    def convert(self):
+        print("아직 구현되지 않음")
 
 root = Tk()
 root.title("Temp Converter")
+root.geometry("250x60+200+200")
 app = App(root)
 
 root.mainloop()
---
+++ gui/t.converter.py
@@ -0,0 +1,14 @@
+# 온도 변환기
+class Converter:
+    def __init__(self, units_from, units_to, factor):
+        self.units_from = units_from
+        self.units_to = units_to
+        self.factor = factor
+        self.offset = offset
+
+    def convert(self, val):
+        return self.factor * val + self.offset
+
+if __name__== "__name__"
+    c1 = Converter("C", 'F', 1.8, 32)
+    print(str(c1.converter(23)) + c1.units_to)
